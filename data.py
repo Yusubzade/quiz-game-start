c4d617e5b6e7d9f07b71f8e6821dad3b508c9f8d
Yusubzade <yusubzade23@gmail.com>
2023-11-12 18:43:19 +0100

    Initial Commit

---
+++ data.py
@@ -0,0 +1,71 @@
+question_data = [
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "Han Solo's co-pilot and best friend,Chewbacca's quot;, is an Ewok.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The movie quot;The Nightmare before Christmas quot; was all done with physical objects.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "Leonardo DiCaprio won an Oscar for Best Actor in 2004,The Aviator.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The film quot;2001: A Space Odyssey quot; was released on December 31st, 2000.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "Matt Damon played an astronaut stranded on an extraterrestrial planet in both of the movies Interstellar and The Martian.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "Ewan McGregor did not know the name of the second prequel film of Star Wars during and after filming.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "In the movie The Revenant, DiCaprio's character hunts down the killer of his son.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "quot;Minions&quot; was released on the June 10th, 2015.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean", "difficulty": "easy",
+        "question": "Actor Tommy Chong served prison time.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }]
